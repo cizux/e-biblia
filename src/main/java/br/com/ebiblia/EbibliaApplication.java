f5b70bad0473770bb3262bd0c94b2ee71e22fe4d
Tarcisio Paz Junger <tarcisiojunger@gmail.com>
2025-01-08 01:13:52 -0300

    Projeto Criado

---
+++ src/main/java/br/com/ebiblia/EbibliaApplication.java
@@ -0,0 +1,13 @@
+package br.com.ebiblia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbibliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbibliaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ebiblia/EbibliaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ebiblia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbibliaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
